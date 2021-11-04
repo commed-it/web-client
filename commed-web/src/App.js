9780e4eadfab164f8ff3b33ce2bee53d8d2eb069
quimpm <quimpm99@gmail.com>
2021-11-04 20:28:54 +0100

    started home page

--- commed-web/src/App.js
+++ commed-web/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
         <Header></Header>
       </div>
       <div className="Body">
-
+        <Home></Home>
       </div>
     </div>
   );
---
+++ commed-web/src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css';
+
+function Home(props) {
+    return (
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div className="homeTopDir col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center align-self-center">
+                    What are you looking for today?
+                </div>
+            </div>
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Home;
