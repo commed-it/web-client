47a9ed0cf72f239cea4daf86726c5cdb9821777e
quimpm <quimpm99@gmail.com>
2021-11-07 17:07:42 +0100

    added communication with backend

--- commed-web/src/components/Home/Products/HomeProducts.js
+++ commed-web/src/components/Home/Products/HomeProducts.js
@@ -7,25 +7,25 @@ function HomeProducts(props) {
 
   const [products, setProducts] = React.useState([]);
 
-  React.useEffect(() => {
-    fetch(configData.SERVER_URL+"/product/",
-      {
-          method : 'GET',
-      })
-      .then(results => results.json())
-      .then(data => {
-        console.log(data.results);
-        setProducts(data.results);
-      });
-    }, []);
+  const getProducts = async () => {
+    const result = await fetch(configData.SERVER_URL+"/product/",
+    {
+        method : 'GET',
+    });
+    const result_json = await result.json();
+    console.log(result_json);
+    setProducts(result_json);
+  }
 
-    console.log(products);
+  React.useEffect(() => {
+      getProducts();
+    }, []);
 
     return (
         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            {products.map((product) => { return(
-                    <div class="card" style="width: 18rem;">
-                      <img class="card-img-top" src={product.images[0]} alt="Product Card Image"></img>
+            {products && products.map((product) => { return(
+                    <div class="card col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                      <img class="card-img-top" src={product.image} alt="Product Card Image"></img>
                       <div class="card-body">
                         <h5 class="card-title">{product.title}</h5>
                         <p class="card-text">{product.description}</p>
