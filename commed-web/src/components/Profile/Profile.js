a99ee97d352e95f209651aba7417838d6f6dce9e
Emina33 <emi.handzic@gmail.com>
2021-11-13 00:39:57 +0100

    profile start

--- commed-web/src/components/Profile/Profile.js
+++ commed-web/src/components/Profile/Profile.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import { useParams } from 'react-router-dom'
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import "./Profile.css";
+import { get } from "../../utils.js";
 
 function Profile(props) {
+  const { userId } = useParams();
 
-    const {userId} = useParams();
+  const [userDetails, setUserDetails] = React.useState([]);
 
-    return (
-        <div>
-            Hello From Profile {userId}!
-        </div>
-    );
+  const getUserDetails = async () => {
+    const result = await get("/user/" + userId + "/", false);
+    const result_json = await result.json();
+    console.log(result_json);
+    setUserDetails(result_json);
+  };
+
+  React.useEffect(() => {
+    getUserDetails();
+  }, []);
+
+  return (
+    <div className="parent3">
+      <div className="container3">
+        <div className="top1">top</div>
+        <img
+          className=" col-xs-12 col-sm-12 col-md-12 col-lg-12 image3"
+          alt="Profile image"
+          src={() => userDetails.images[0].image}
+        ></img>
+        <div className="bottom1">bottom</div>
+      </div>
+      <div className="decription3">
+        <h5>Description</h5>
+        <p>sheeeeeeeeeshe</p>
+      </div>
+      Hello From Profile {userId}!
+    </div>
+  );
 }
 
 export default Profile;
