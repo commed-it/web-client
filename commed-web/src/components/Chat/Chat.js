e54e2a0229ba159c66c4cf77e8b1fb5c82953d55
Emina33 <emi.handzic@gmail.com>
2021-12-12 20:36:22 +0100

    sry

--- commed-web/src/components/Chat/Chat.js
+++ commed-web/src/components/Chat/Chat.js
@@ -97,25 +97,33 @@ function Chat(props) {
           </svg>{" "}
           <h5>My profile</h5>
         </div>
-        <div className="partner">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="35"
-            height="35"
-            fill="#373843"
-            className="bi bi-person-circle centerIcon"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-            <path
-              fill-rule="evenodd"
-              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-            />{" "}
-          </svg>{" "}
-          <button class="formalOfferButton mt-auto btn btn-primary">
-            Formal offer
-          </button>
-        </div>
+        {chat != "" &&
+          encounters &&
+          encounters.map((encounter) => {
+            if (
+              encounter.id == chat &&
+              encounter.product.owner == logedUser.pk
+            ) {
+              return (
+                <div className="partner">
+                  <h5>{encounter.client.name}</h5>
+                  <button class="formalOfferButton mt-auto btn btn-primary">
+                    Formal offer
+                  </button>
+                </div>
+              );
+            }
+            if (
+              encounter.id == chat &&
+              encounter.client.owner == logedUser.pk
+            ) {
+              return (
+                <div className="partner">
+                  <h5>{encounter.product.owner}</h5>
+                </div>
+              );
+            }
+          })}
       </div>
       <div className="chatContent">
         <div className="chatContentLeft">
