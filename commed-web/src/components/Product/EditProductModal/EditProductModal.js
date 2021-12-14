6ab8987b5e14da9dbfbbbed22c7aadf092031b35
quimpm <quimpm99@gmail.com>
2021-12-14 10:56:51 +0100

    added labels to edit modals

--- commed-web/src/components/Product/EditProductModal/EditProductModal.js
+++ commed-web/src/components/Product/EditProductModal/EditProductModal.js
@@ -159,6 +159,7 @@ function EditProductModal(props) {
         </div>
         {getComponent()}
         <div className="modal-body mx-3 border-0">
+          Title:
           <div class="md-form mb-5">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ced4da" class="bi bi-sticky icons" viewBox="0 0 16 16">
             <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>            </svg>
@@ -170,6 +171,7 @@ function EditProductModal(props) {
               value = {title}
             ></input>
           </div>
+          Description:
           <div class="md-form mb-5">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -190,6 +192,7 @@ function EditProductModal(props) {
               value = {description}
             ></textarea>
           </div>
+          Images:
           <div className="md-form mb-5">
             <Carousel activeIndex={index} onSelect={handleSelect}>
                 { images && 
@@ -231,6 +234,7 @@ function EditProductModal(props) {
                 }
             </div>
           </div>
+          Tags:
           <div className="md-form mb-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -255,6 +259,7 @@ function EditProductModal(props) {
               htmlFor="defaultForm-pass"
             ></label>
           </div>
+          Latitude:
           <div class="md-form mb-5">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ced4da" class="bi bi-sticky icons" viewBox="0 0 16 16">
             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>            
@@ -267,6 +272,7 @@ function EditProductModal(props) {
               value = {latitude}
             ></input>
           </div>
+          Longitude:
           <div class="md-form mb-5">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ced4da" class="bi bi-sticky icons" viewBox="0 0 16 16">
             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>            
--- commed-web/src/components/Profile/EditProfileModal/EditProfileModal.js
+++ commed-web/src/components/Profile/EditProfileModal/EditProfileModal.js
@@ -89,6 +89,7 @@ function EditProfileModal(props) {
                 </h4>
                 </div>
                 <div className="modal-body mx-3 border-0">
+                    Name:
                     <div className="md-form mb-4">
                         <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -108,6 +109,7 @@ function EditProfileModal(props) {
                         value = {name}
                         />
                     </div>
+                    NIF:
                     <div className="md-form mb-4">
                         <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -127,6 +129,7 @@ function EditProfileModal(props) {
                         value = {nif}
                         />
                     </div>
+                    Contact Info:
                     <div class="md-form mb-5">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ced4da" class="bi bi-sticky icons" viewBox="0 0 16 16">
                         <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>            
@@ -139,6 +142,7 @@ function EditProfileModal(props) {
                         value = {contactInfo}
                         ></input>
                     </div>
+                    Description:
                     <div class="md-form mb-5">
                         <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -159,6 +163,7 @@ function EditProfileModal(props) {
                         value = {description}
                         ></textarea>
                     </div>
+                    Profile Image:
                     <div class="md-form mb-5">
                         <input
                         type="file"
@@ -167,6 +172,7 @@ function EditProfileModal(props) {
                         onChange={handleProfileImage}
                         ></input>
                     </div>
+                    Banner Image:
                     <div class="md-form mb-5">
                         <input
                         type="file"
