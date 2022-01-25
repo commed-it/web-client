b49cec61f1db1c39b20d05b04912b416aae7990f
quimpm <quimpm99@gmail.com>
2022-01-25 21:00:30 +0100

    style changes 2.0

--- commed-web/src/components/Signature/Signature.js
+++ commed-web/src/components/Signature/Signature.js
@@ -68,11 +68,15 @@ function Signature(props) {
                             </button>
                         </div>
                         <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center align-items-center divPagesAndButtons'>
-                            <button className='btn buttonPages col-xs-4 col-sm-3 col-md-2 col-lg-2' onClick={handlePrevPage}>Previous</button>
+                            <button className='btn buttonPagesChange col-xs-4 col-sm-3 col-md-2 col-lg-2' onClick={handlePrevPage}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill iconSwitch" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                            </svg></button>
                             <div className='labelPages d-flex justify-content-center align-items-center'>
                                 Page {pageNumber} of {numPages}
                             </div>
-                            <button className='btn buttonPages col-xs-4 col-sm-3 col-md-2 col-lg-2' onClick={handleNextPage} >Next</button>
+                            <button className='btn buttonPagesChange col-xs-4 col-sm-3 col-md-2 col-lg-2' onClick={handleNextPage} ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill iconSwitch" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+                            </svg></button>
                         </div>
                         <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center align-items-center divPagesAndButtons'>
                             <a href={configData.SERVER_URL + formalOffers.formalOffer.pdf} target="_blank" class="aDownload" >
